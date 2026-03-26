1e6eaaf5116da8f03da928f361cf976536b82d6e
rajadaud12 <rajadaud12@users.noreply.github.com>
2026-03-26 00:51:14 +0000

    deploy: e1c3b0b204c24408c4b514e1425bf0a2fdac2e8b

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "775843519"
+    serviceWorkerVersion: "4164356693"
   }
 });
