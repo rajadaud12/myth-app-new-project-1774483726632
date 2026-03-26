8d56625f3161af21a0621cff0a52c6c5503a153b
Daud Bin Nasar <64163448+rajadaud12@users.noreply.github.com>
2026-03-26 05:08:54 +0500

    Update lib/utils/colors.dart via MYTH Build System

---
+++ lib/utils/colors.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color gradientStart = Color(0xFF0A0A5C);
+  static const Color gradientMid = Color(0xFF0D0D3B);
+  static const Color gradientEnd = Color(0xFF000000);
+  static const Color cardBackground = Color(0xFF1C1C1E);
+  static const Color positiveAmount = Color(0xFF4CD964);
+  static const Color negativeAmount = Color(0xFFFF3B30);
+  static const Color primaryText = Colors.white;
+  static const Color secondaryText = Color(0xFF8E8E93);
+  static const Color accentBlue = Color(0xFF2979FF);
+  static const Color buttonBorder = Color(0xFF3A3A3C);
+  static const Color iconBackground = Color(0xFF2C2C2E);
+  static const Color bottomNavBackground = Color(0xFF1C1C1E);
+  static const Color selectedNavItem = Colors.white;
+  static const Color unselectedNavItem = Color(0xFF636366);
+}
